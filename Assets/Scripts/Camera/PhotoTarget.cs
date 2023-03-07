022c2c23d47f1cada4d6e89263f5630ca65ee87c
Shchinnikov <mr.shchinnikov@mail.ru>
2023-03-07 19:19:12 +0400

    added scripts for the camera. Both are not working yet.

---
+++ Assets/Scripts/Camera/PhotoTarget.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoTarget : MonoBehaviour
+{
+    //[SerializeField] private string AnimalName = "aboba";
+    //private Renderer _renderer;
+
+    //private void Start()
+    //{
+    //    _renderer = GetComponentInChildren<Renderer>();
+    //}
+
+    //private void OnRenderObject()
+    //{
+    //    if (CameraManager.IsTakingShot)
+    //    {
+    //        var tex = CameraManager.OutputTexture;
+    //        var screenPos = CameraManager.CurrentCamera.Camera
+    //            .WorldToScreenPoint(_renderer.bounds.center, Camera.MonoOrStereoscopicEye.Mono);
+
+    //        if (screenPos.z < CameraManager.CurrentCamera.Camera.nearClipPlane || 
+    //            screenPos.z > CameraManager.MaxDistToObject) 
+    //            return;
+
+    //        var screenCenter = new Vector2(tex.width/2, tex.height/2);
+
+    //        var dist = Vector2.Distance(screenCenter, screenPos);
+    //        if (Mathf.Abs(dist) > screenCenter.magnitude/2)
+    //        {
+    //            return;
+    //        }
+
+    //        GetPhotographed();
+    //    }
+    //}
+
+    //public void GetPhotographed()
+    //{
+    //    Debug.Log($"{AnimalName} got photographed");
+    //}
+}
