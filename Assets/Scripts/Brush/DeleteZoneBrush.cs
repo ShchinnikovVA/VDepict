21834f89aa911a3d79206fa1cc5a5e79d140a1fc
Shchinnikov <mr.shchinnikov@mail.ru>
2023-04-22 19:36:29 +0400

    added DeleteZoneBrush ничерта не работает, как надо

---
+++ Assets/Scripts/Brush/DeleteZoneBrush.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteZoneBrush : MonoBehaviour
+{
+    public float radius = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (radius != GetComponent<SphereCollider>().radius)
+        {
+            GetComponent<SphereCollider>().radius = radius; // думаю, это можно лучше сделать. Если игрок меняет радиус у себя, то меняется и у тригера
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " удалён");
+        //if (Input.GetKey(KeyCode.E)) //поменять на кнопку контроллера
+        //{
+            //Debug.Log("кнопка Е нажата");
+            if (other.tag == "Paint")
+            {
+                Destroy(other.gameObject);
+            }
+        //}
+    }
+}
