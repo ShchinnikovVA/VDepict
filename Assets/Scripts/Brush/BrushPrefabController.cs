8791fadaa032b9183afba8141743a0f4de3c0061
Shchinnikov <mr.shchinnikov@mail.ru>
2023-04-23 21:55:43 +0400

    fixed brush create function

--- Assets/Scripts/Brush/BrushPrefabController.cs
+++ Assets/Scripts/Brush/BrushPrefabController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BrushPrefabController : MonoBehaviour
 {
     public Color color;
     public float size;
     public float minDistance;
     public GameObject brushPrefab;
+    public GameObject lookTarget;
 
     public void OnClickButton()
     {
@@ -17,9 +19,11 @@ public class BrushPrefabController : MonoBehaviour
     public void CreateParticleDot(float _size, float _minDistance, GameObject _brushPrefab, Color _color)
     {
         var _scale = new Vector3(_size, _size, _size);
+        Vector3 _locate = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
 
-        _brushPrefab.transform.GetChild(0).GetComponent<Material>().SetColor("kavo", _color);
+        _brushPrefab.transform.GetComponent<PointLookAtTarget>().target = lookTarget.transform;
+        _brushPrefab.transform.GetChild(0).GetComponent<SpriteRenderer>().color = _color;
         _brushPrefab.transform.localScale = _scale;
-        Instantiate(_brushPrefab, gameObject.transform);
+        Instantiate(_brushPrefab, _locate, new Quaternion(0,0,0,0));
     }
 }
