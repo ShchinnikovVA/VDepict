e34a7ef8bef203137569a70c46181414cc913140
Shchinnikov <mr.shchinnikov@mail.ru>
2023-05-04 14:24:36 +0400

    Added test colored skies

--- Assets/Scripts/UI/SceneSettingsGroup.cs
+++ Assets/Scripts/UI/SceneSettingsGroup.cs
@@ -65,18 +65,9 @@ public class SceneSettingsGroup : MonoBehaviour
     private float[] ColorComparison(Color newColor, Color oldColor) // запись цветов небес
     {
         float[] count = {0, 0, 0};
-        if (newColor.r < oldColor.r)
-        {
-            count[0] = (oldColor.r - newColor.r) / step;
-            count[1] = (oldColor.g - newColor.g) / step;
-            count[2] = (oldColor.b - newColor.b) / step;
-        }
-        else if (newColor.r > oldColor.r)
-        {
-            count[0] = (newColor.r - oldColor.r) / step;
-            count[1] = (newColor.g - oldColor.g) / step;
-            count[2] = (newColor.b - oldColor.b) / step;
-        }
+        count[0] = (newColor.r - oldColor.r) / step;
+        count[1] = (newColor.g - oldColor.g) / step;
+        count[2] = (newColor.b - oldColor.b) / step;
         return count;
     }
     IEnumerator SwitchColor(float[] countTop, float[] countMid, float[] countBot, bool isUpColor)
@@ -85,20 +76,12 @@ public class SceneSettingsGroup : MonoBehaviour
         var _top = mainSky.GetColor(topName); // локальная запись значений цветов текущего неба
         var _mid = mainSky.GetColor(midName);
         var _bot = mainSky.GetColor(botName);
-        if (isUpColor) // изменение неба и пола на полученное значение
-        {
-            mainSky.SetColor(topName, new Color(_top.r + countTop[0], _top.g + countTop[1], _top.b + countTop[2]));
-            mainSky.SetColor(midName, new Color(_mid.r + countMid[0], _mid.g + countMid[1], _mid.b + countMid[2]));
-            mainSky.SetColor(botName, new Color(_bot.r + countBot[0], _bot.g + countBot[1], _bot.b + countBot[2]));
-            floorMaterial.color = new Color(floorMaterial.color.r + _floorColors[0], floorMaterial.color.g + _floorColors[1], floorMaterial.color.b + _floorColors[2]);
-        }
-        else
-        {
-            mainSky.SetColor(topName, new Color(_top.r - countTop[0], _top.g - countTop[1], _top.b - countTop[2]));
-            mainSky.SetColor(midName, new Color(_mid.r - countMid[0], _mid.g - countMid[1], _mid.b - countMid[2]));
-            mainSky.SetColor(botName, new Color(_bot.r - countBot[0], _bot.g - countBot[1], _bot.b - countBot[2]));
-            floorMaterial.color = new Color(floorMaterial.color.r - _floorColors[0], floorMaterial.color.g - _floorColors[1], floorMaterial.color.b - _floorColors[2]);
-        }
+        // изменение неба и пола на полученное значение
+        mainSky.SetColor(topName, new Color(_top.r + countTop[0], _top.g + countTop[1], _top.b + countTop[2]));
+        mainSky.SetColor(midName, new Color(_mid.r + countMid[0], _mid.g + countMid[1], _mid.b + countMid[2]));
+        mainSky.SetColor(botName, new Color(_bot.r + countBot[0], _bot.g + countBot[1], _bot.b + countBot[2]));
+        floorMaterial.color = new Color(floorMaterial.color.r + _floorColors[0], floorMaterial.color.g + _floorColors[1], floorMaterial.color.b + _floorColors[2]);
+        
         yield return new WaitForSeconds(switchTime);
         if (step > 0) // делаем это определённое количество раз, чтобы переход был более плавный
         {
